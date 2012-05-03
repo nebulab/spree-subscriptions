6bc11fdcf6f29b3e6a14cbc721563f66c63fbf69
Alberto Vena <kennyadsl@gmail.com>
2012-05-03 18:14:20 +0200

    fixes error on pending test

--- spec/models/spree/subscription_spec.rb
+++ spec/models/spree/subscription_spec.rb
@@ -22,16 +22,10 @@ describe Spree::Subscription do
     Spree::Subscription.find(:first, :conditions => {:variant_id => @order.line_items.first.variant }).should_not be_nil
   end
 
-  it "should be created with pending status if payment is not completed" do
-    pending "test that if order is not paid the subscription is created with pendig status"
-  end
+  it "should be created with pending status if payment is not completed" 
 
-  it "should be have active status if order is paid" do
-    pending "test that if an order is completed and paid, subscription is active"
-  end
+  it "should be have active status if order is paid" 
 
   it "should be renewed on payment completion if already exists for that user"
-    pending "on payment completion an existing subscription should be renewed"
-  end
 
 end
