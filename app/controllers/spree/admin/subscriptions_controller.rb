ac3bd6d52765a0ca74abe140b140bfd5e0eb9e58
Alberto Vena <kennyadsl@gmail.com>
2012-05-03 13:40:06 +0200

    basic model and controller for subscription

--- app/controllers/spree/admin/subscriptions_controller.rb
+++ app/controllers/spree/admin/subscriptions_controller.rb
@@ -1,2 +1,15 @@
-class Spree::SubscriptionsController < ApplicationController
+module Spree
+  module Admin
+    class SubscriptionsController < Spree::Admin::BaseController
+      respond_to :html
+
+      def index 
+        params[:q] ||= {}
+        @search = Subscription.search(params[:q])
+        @subscriptions = @search.result.includes([:user]).page(params[:page]).per(Spree::Config[:orders_per_page])
+        respond_with(@subscriptions)
+      end
+
+    end
+  end
 end
--- app/models/spree/subscription.rb
+++ app/models/spree/subscription.rb
@@ -3,5 +3,19 @@ class Spree::Subscription < ActiveRecord::Base
   
   belongs_to :user
   belongs_to :variant
+  
+  state_machine :state, :initial => 'pending' do
+    event :cancel do
+      transition :to => 'canceled', :if => :allow_cancel?
+    end
+
+    event :activate do
+      transition :to => 'activated', :from => 'pending'
+    end
+  end
+
+  def allow_cancel?
+    self.state != 'canceled'
+  end
 
 end
