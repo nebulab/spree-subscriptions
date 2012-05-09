4d2f4d74fd4e076856612bfa3d49aacf146929ff
Alberto Vena <kennyadsl@gmail.com>
2012-05-09 19:51:12 +0200

    refactor variant model tests

--- spec/models/spree/variant_spec.rb
+++ spec/models/spree/variant_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
 describe Spree::Variant do
-
-  it "should respond to subscribable? method" do
-    variant = Factory(:subscribable_variant)
-    variant.should respond_to :subscribable?
+  before(:each) do
+    @variant = Factory(:subscribable_variant)
   end
 
+  it "should respond to subscribable? method" do
+    @variant.should respond_to :subscribable?
+  end
+
+  it "should be subscribable" do
+    @variant.subscribable.should == true
+  end
 end
