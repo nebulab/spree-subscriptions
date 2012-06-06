0b234ca8072ae282d85c10aac8f7034e2a17f5bc
Matteo Latini <mtylty@gmail.com>
2012-06-06 09:50:48 +0200

    refactors decorator syntax to be uniform

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -1,8 +1,10 @@
-Spree::UsersController.class_eval do 
-  before_filter :include_user_subscriptions, :only => :show
+module Spree
+  UsersController.class_eval do 
+    before_filter :include_user_subscriptions, :only => :show
 
-  private
-  def include_user_subscriptions
-    @subscriptions = Spree::Subscription.find(:all, :conditions => { :email => @user.email })
+    private
+    def include_user_subscriptions
+      @subscriptions = Spree::Subscription.find(:all, :conditions => { :email => @user.email })
+    end
   end
 end
--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,9 +1,11 @@
-Spree::Payment.class_eval do
-  state_machine :initial => 'checkout' do
-    after_transition :to => 'completed', :do => :activate_subscriptions!
-  end
+module Spree
+  Payment.class_eval do
+    state_machine :initial => 'checkout' do
+      after_transition :to => 'completed', :do => :activate_subscriptions!
+    end
 
-  def activate_subscriptions!
-    self.order.activate_subscriptions
+    def activate_subscriptions!
+      self.order.activate_subscriptions
+    end
   end
 end
