d032726c1fa9756f8fb0c744d198a21b6315ec62
Alberto Vena <kennyadsl@gmail.com>
2012-06-09 09:59:28 +0200

    define basic grid for addresses labels

--- app/pdfs/issue_pdf.rb
+++ app/pdfs/issue_pdf.rb
@@ -1,13 +1,36 @@
 class IssuePdf < Prawn::Document
   def initialize(issue, view)
-    super(top_margin: 70)
+    super(left_margin: 16, right_margin: 16, page_size: 'A4')
     @issue = issue
     @view = view
     address
   end
   
   def address
-    text "address_goes_here", size: 30, style: :bold
+    print_page_labels 
+    start_new_page
+    print_page_labels
   end  
-  
+
+  def print_page_labels
+    define_grid(columns: 3, rows: 10, column_gutter: 12, row_gutter: 10)
+    grid.rows.times do |row|
+      grid.columns.times do |column|
+        cell = grid(row, column)
+        self.bounding_box(cell.top_left, width: cell.width, height: cell.height) do 
+          indent 7 do
+            move_down 7                 
+            text "Name"
+            text "Address"
+            text "Address 2"
+            text "City CAP State"
+          end
+          stroke do
+            rectangle(bounds.top_left, cell.width, cell.height)
+          end
+        end
+      end
+    end
+  end
+
 end
