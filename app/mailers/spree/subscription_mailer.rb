0f4e35d247006ecfae291d7a83df976c8658bdb4
Matteo Latini <mtylty@gmail.com>
2012-09-18 13:03:09 +0200

    removes default from field for mails

--- app/mailers/spree/subscription_mailer.rb
+++ app/mailers/spree/subscription_mailer.rb
@@ -1,6 +1,4 @@
 class Spree::SubscriptionMailer < ActionMailer::Base
-  default from: "from@example.com"
-
   def subscription_ending_email(subscription)
     @subscribed_by = subscription.email
     mail(:to => @subscribed_by, :subject => t(:subscription_ending))
