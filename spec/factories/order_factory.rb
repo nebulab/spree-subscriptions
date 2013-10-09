8524e2a751e5800503794f0d05eff0d48971db0e
Alberto Vena <kennyadsl@gmail.com>
2013-10-09 17:06:04 +0100

    updates after_create FactoryGirl syntax

--- spec/factories/order_factory.rb
+++ spec/factories/order_factory.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
   factory :order_with_subscription, :parent => :order do
-    after_create do |order|
-      order.line_items << create(:line_item, :order => order,
-              :variant => create(:subscribable_variant))
+    after(:create) do |order|
+      order.line_items << create(:line_item, :order => order, :variant => create(:subscribable_variant))
     end
   end
 end
