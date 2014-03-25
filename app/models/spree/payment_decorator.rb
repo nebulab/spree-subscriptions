b91504c68502eff5425cf39b530973eac5e561c0
Ryan Bigg <radarlistener@gmail.com>
2014-03-25 16:07:13 +1100

    Use symbols for payment decorator states
    By re-defining the state machine like this, the state_machine gem will complain because 'checkout' != :checkout and 'completed' != :completed

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,7 +1,7 @@
 module Spree
   Payment.class_eval do
-    state_machine :initial => 'checkout' do
-      after_transition :to => 'completed', :do => :create_subscriptions!
+    state_machine :initial => :checkout do
+      after_transition :to => :completed, :do => :create_subscriptions!
     end
 
     def create_subscriptions!
