0469c99e4c6487bda20ae838711d7d00b56a30d2
Steven Barragan <me@steven.mx>
2014-04-10 16:38:20 -0500

    Point admin menu override to the new location

--- app/overrides/subscriptions_admin_tab.rb
+++ app/overrides/subscriptions_admin_tab.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "subscriptions_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
                      :text => "<%= tab(:subscriptions, :icon => 'icon-file') %>")
