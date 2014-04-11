e1e3ec07729b18380ce151b2d8fa723addeb017f
Steven Barragan <me@steven.mx>
2014-04-11 13:54:37 -0500

    Add strong params to CustomerdetailsController

--- app/controllers/spree/admin/subscriptions/customer_details_controller.rb
+++ app/controllers/spree/admin/subscriptions/customer_details_controller.rb
@@ -12,9 +12,9 @@ module Spree
         def edit
           @subscription.build_ship_address(:country_id => Spree::Config[:default_country_id]) if @subscription.ship_address.nil?
         end
-        
+
         def update
-          if @subscription.update_attributes(params[:subscription])
+          if @subscription.update_attributes(customer_params)
             flash[:notice] = t('customer_details_updated')
             redirect_to edit_admin_subscription_path(@subscription)
           else
@@ -23,9 +23,26 @@ module Spree
         end
 
         private
-        
+
         def load_subscription
           @subscription = Subscription.find(params[:subscription_id])
+        end
+
+        def customer_params
+          params.require(:subscription).
+            permit(:email, ship_address_attributes: [
+                :firstname,
+                :lastname,
+                :address1,
+                :address2,
+                :city,
+                :zipcode,
+                :state_id,
+                :country_id,
+                :phone,
+                :id
+              ]
+            )
         end
       end
     end
