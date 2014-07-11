a902d439878a67ee9ad0c078ad1058662fbf1863
Alberto Vena <kennyadsl@gmail.com>
2014-07-11 15:16:06 +0200

    fixes migrations not being installed, #29 related

--- lib/spree/subscriptions_configuration.rb
+++
@@ -1,4 +0,0 @@
-class Spree::SubscriptionsConfiguration < Spree::Preferences::Configuration
-  preference :use_delayed_job, :boolean, :default => true
-  preference :default_issues_number, :integer, :default => 12
-end
