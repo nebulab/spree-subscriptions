3bd9fbe04cd835b0d74d1c483ce8fb45e7ad6903
brchristian <brchristian@gmail.com>
2014-07-30 21:06:49 -0400

    Shortcut for ShippedIssue#magazine_issue
    Here we can write `ShippedIssue.magazine_issue` rather than always needing to write `ShippedIssue.issue.magazine_issue`, similar to the way that Spree uses  `LineItem.product` as a shortcut for `LineItem.variant.product`.

--- app/models/spree/shipped_issue.rb
+++ app/models/spree/shipped_issue.rb
@@ -1,6 +1,7 @@
 class Spree::ShippedIssue < ActiveRecord::Base
   belongs_to :issue, :autosave => true
   belongs_to :subscription, :autosave => true
+  has_one :magazine_issue, through: :issue
 
   #attr_accessible :subscription, :issue
 end
