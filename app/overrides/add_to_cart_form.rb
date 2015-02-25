acd772c1be3ec238b6d6c6548b245cf4105b08a4
acreilly <acreilly3@gmail.com>
2015-02-25 11:39:50 -0500

    fixed locales in view

--- app/overrides/add_to_cart_form.rb
+++ app/overrides/add_to_cart_form.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(virtual_path: "spree/products/_cart_form",
   name: "use_subscribe_locale_instead_of_add_to_cart",
   replace: "erb[loud]:contains(\"Spree.t(:add_to_cart)\")",
-  text: "<%= @product.subscribable? ? t(:subscribe_call_to_action) : Spree.t(:add_to_cart) %>"
+  text: "<%= @product.subscribable? ? Spree.t(:subscribe_call_to_action) : Spree.t(:add_to_cart) %>"
   )
 
 Deface::Override.new(virtual_path: "spree/products/_cart_form",
